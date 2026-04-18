448cf9413ed2407c8387342d5b4047dc34324982
guppy <82202242+guppyadrian@users.noreply.github.com>
2026-04-18 15:48:58 -0700

    add SFML and box2d

--- src/PA9.cpp
+++ src/PA9.cpp
@@ -3,8 +3,28 @@
 
 #include <iostream>
 
+#include "SFML/Graphics/CircleShape.hpp"
+#include "SFML/Graphics/RenderWindow.hpp"
+#include "SFML/Window/Window.hpp"
+
 int main()
 {
-    std::cout << "Hello World!" << std::endl;
-    return 0;
+
+    sf::RenderWindow window(sf::VideoMode({600, 400}), "Game");
+
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen())
+    {
+        while (const std::optional event = window.pollEvent())
+        {
+            if (event->is<sf::Event::Closed>())
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
 }
