1c57df5a475054165c3fe6c31e4c53309e68e762
Sprink02 <johnathanemanuelli3@gmail.com>
2026-04-19 18:45:50 -0700

    minorchanges
    changes to LevelScene to have init and update made

--- src/LevelScene.cpp
+++ src/LevelScene.cpp
@@ -1,3 +1,19 @@
 #include "LevelScene.hpp"
 
 LevelScene::LevelScene() : _gameObjects() {}
+
+void LevelScene::initialize()
+{
+	for (int i = 0; i < _gameObjects.size(); i++)
+	{
+		_gameObjects[i].initialize(); //TODO
+	}
+}
+
+void LevelScene::update()
+{
+	for (int i = 0; i < _gameObjects.size(); i++)
+	{
+		_gameObjects[i].update(); //TODO
+	}
+}
